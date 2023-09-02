cee35145c8e2383b7658beff6a80387e4c2bf7d4
colin fitzgerald <colin@sonrai.io>
2023-09-01 22:24:07 -0500

    Update

--- ExtRSAuth/Properties/Settings.Designer.cs
+++ ExtRSAuth/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Sonrai.ExtRSAuth.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace Sonrai.ExtRSAuth.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("[Your 256bit AES symetric encryption key]")]
+        [global::System.Configuration.DefaultSettingValueAttribute("R+yBCm/SGCTtuQ76SNzN+wijxol6TC1RIXjFTL2MtpQ=")]
         public string cle {
             get {
                 return ((string)(this["cle"]));

75e1c13dc0427abc52f25e1d7e4cfe2ccdc7ccc1
colin fitzgerald <colin@sonrai.io>
2025-07-12 17:14:22 -0500

    Update ExtRsUser ref

--- ExtRSAuth/AuthenticationUtilities.cs
+++ ExtRSAuth/AuthenticationUtilities.cs
@@ -30,7 +30,6 @@ namespace Sonrai.ExtRSAuth
     {
         public const string ExtRsUser = "extRSAuth";
         public const bool NoAuth = false; // ONLY set == true when initializing extRSAuth Admin user
-        public const string ExtRsReadOnlyUser = "extRS.Portal";
         public const string ReadOnlyUser = "BUILTIN\\Everyone";
         public const string MSBIToolsUser = "ReportingServicesTools";
         public const string ReportExecution2005SOAP = "https://localhost/reportserver/ReportExecution2005.asmx";
